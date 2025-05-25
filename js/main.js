7e881fd542633c661b209ad397c5d1fbfe4b58f7
perfectcell88 <zwaow88@gmail.com>
2025-05-25 21:47:11 +1000

    Update main.js

--- js/main.js
+++ js/main.js
@@ -2,6 +2,8 @@ import * as THREE from './three.module.js';
 import { OrbitControls } from './controls/OrbitControls.js';
 import { Water } from './objects/Water.js';
 import { Sky } from './objects/Sky.js';
+import { Lensflare, LensflareElement } from './objects/Lensflare.js';
+
 
 let container;
 let camera, scene, renderer;
